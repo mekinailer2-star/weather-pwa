415be448e9d0533579d22677dac5092d6c11e00b
mekinailer2-star <mekinailer2@gmail.com>
2026-04-06 03:50:01 +0300

    Add config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+    API_KEY: 'BURAYA_API_KEY_YAZIN',
+    BASE_URL: 'https://api.openweathermap.org/data/2.5',
+    GEO_URL: 'https://api.openweathermap.org/geo/1.0',
+    DEFAULT_CITY: 'Istanbul',
+    DEFAULT_LANG: 'tr',
+    DEFAULT_UNITS: 'metric',
+    CACHE_DURATION: 10 * 60 * 1000,
+    MAX_HISTORY: 10,
+    MAX_FAVORITES: 20
+};
